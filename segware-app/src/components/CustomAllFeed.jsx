59b5690b8dc61ccc562cba5bc9ee16700de5c3f3
marcelo ivan <sagacello@hotmail.com>
2021-06-25 14:04:18 -0300

    tela completa e os upvotes estão respondendo corretamente

--- segware-app/src/components/CustomAllFeed.jsx
+++ segware-app/src/components/CustomAllFeed.jsx
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Item, Icon } from 'semantic-ui-react';
+import { Item, Icon, Segment, Container, Divider } from 'semantic-ui-react';
 import CustomSubHeader from './CustomSubHeader';
-const CustomAllFeed = ({ index, item, marked }) => {
+
+const CustomAllFeed = ({
+  index,
+  item,
+  key,
+}) => {
+  const [markedLike, setMarkedLike] = useState(false);
+  const [markedHeart, setMarkedHeart] = useState(false);
+  useEffect(() => {}, [setMarkedLike]);
+  useEffect(() => {}, [setMarkedHeart]);
+  
+  
+  const handleSubmitLike = (e) => {
+    setMarkedLike(!markedLike);
+    // cada veze que eu clico eu nego o estado e atualizo
+  };
+  const handleSubmitHeart = (e) => {
+    setMarkedHeart(!markedHeart);
+  };
+ 
   return (
-    <Item.Group>
-      <Item style={{ marginTop: '3vh' }}>
-        <Item.Content>
-          <CustomSubHeader message={`TEXTO ${index + 1}`}></CustomSubHeader>
-          <p style={({ color: 'whitesmoke' })}>
-            {item}
-          </p>
-          <Item.Extra>
-            <Icon
-              rotated="counterclockwise"
-              color="red"
-              size="large"
-              style={{ marginRight: '1vh' }}
-              name="like"
-            />
-            <Icon
-              rotated="counterclockwise"
-              color="yellow"
-              size="large"
-              name="favorite"
-            />
-          </Item.Extra>
-        </Item.Content>
-      </Item>
-    </Item.Group>
+    <Segment inverted>
+      <CustomSubHeader message={`TEXTO ${index + 1}`}></CustomSubHeader>
+      <Item.Group>
+        <Container
+          fluid
+          textAlign="justified"
+          style={{ color: 'whitesmoke' }}
+          text="true"
+        >
+          <p>{item.content}</p>
+        </Container>
+        <Divider />
+        <Item>
+          <Item.Content>
+            <Item.Extra>
+              <Icon
+                rotated={markedHeart ? 'counterclockwise' : 'clockwise'}
+                color={markedHeart ? 'red' : 'grey'}
+                size="big"
+                style={{ marginRight: '30vh' }}
+                name="like"
+                onClick={ (e) => handleSubmitHeart(e)}
+          
+              />
+              <Icon
+                rotated={markedLike ? 'clockwise' : 'counterclockwise'}
+                color={markedLike ? 'yellow' : 'grey'}
+                size="big"
+                name="favorite"
+                onClick={(e) => handleSubmitLike(e)}
+              />
+            </Item.Extra>
+          </Item.Content>
+        </Item>
+      </Item.Group>
+    </Segment>
   );
 };
 
